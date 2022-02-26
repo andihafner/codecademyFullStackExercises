f2f45fdf98f703ee8014c7b254cd24fd477c7def
Andi Hafner <andihafner@andihafner.com>
2022-02-26 19:09:27 +0100

    lodash rebuild: Implement has, invert, findKey and drop method

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 This repo contains some exercises I worked through while getting through the [**Full Stack Engineer Path 2022** of codecademy.com](https://www.codecademy.com/learn/paths/full-stack-engineer-career-path).
 
 I publish them here for other course participants for comparison purposes.
-
-_Files which I mainly created or modified myself_ or folders which contain some of them are prefixed with my initials **_ah_**.
-
--- javaScript/lodashMethodsRebuild/_.js
+++ javaScript/lodashMethodsRebuild/_.js
@@ -1,4 +1,4 @@
-/* _.js, codecademy.com full-stack engineer career path exercise,
+/* _.js, codecademy.com full stack engineer career path exercise,
     andihafner.com 2022
     
     The course exercises demand to re-implement some lodash methods.
@@ -9,9 +9,17 @@
 
 let _ = {
     clamp(number, lower, upper) {
+
+        /* The clamp method returns the nearest value of a given number
+            which fits in a given range. */
+
         return Math.min(Math.max(number, lower), upper);
     },
     inRange(number, startValue, endValue) {
+
+        /* The inRange method Checks if a number is between a start and up to,
+            but not including, an end value. */
+
         // If no end value is given
         if (endValue === undefined) {
             endValue = startValue;
@@ -23,26 +31,93 @@ let _ = {
             startValue = endValue;
             endValue = newEndValue;
         }
-        if (startValue <= number && number < endValue) { return true }
-        return false
+        return startValue <= number && number < endValue ? true : false;
     },
     words(string) {
+
+        /* The words method takes in a string an returns an array with
+            each word as an own element, using space chars as delimiter */
+
         return string.split(' ');
     },
     pad(string, desiredStringLength) {
+
+        /* The pad method expands a given string to the desired
+            length by filling in spaces.
+            
+           If the string is already longer than the desired new length,
+            the string leaves untouched. */
+
         const stringLength = string.length;
-        if (desiredStringLength < stringLength) {return string}
+        // Return string unmodified if its length already exceeds
+        if (desiredStringLength < stringLength) {
+            return string
+        }
         const totalSpacesCount = desiredStringLength - stringLength;
         const totalSpacesCountIsEven = !(desiredStringLength % 2);
         const halfSpacesCount = Math.floor(totalSpacesCount / 2);
         const stringPrefix = ' '.repeat(halfSpacesCount);
         let stringSuffix = stringPrefix;
-        if (!totalSpacesCountIsEven) {stringSuffix += ' '}
+        // Add additional space character at the end if needed
+        if (!totalSpacesCountIsEven) {
+            stringSuffix += ' '
+        }
         return `${stringPrefix}${string}${stringSuffix}`
+    },
+    has(object, key) {
+
+        /* The has method checks a given object for the existence of
+            a given key. */
+
+        return (object[key] != undefined ? true : false)
+    },
+    invert(object) {
+
+        /* The invert method returns a given object with swapped
+            key: value pairs. */
+
+        const invertedObject = {};
+        let originalKey = '';
+        for (let [key, value] of Object.entries(object)) {
+            originalKey = key;
+            key = value;
+            value = originalKey;
+            invertedObject[key] = value;
+        }
+        return invertedObject;
+    },
+    findKey(object, predicateFunction) {
+
+        /* The findKey method returns the key of the first value which matches
+            the predicateFunction (a function, which returns true of false).
+
+        Example for a predicateFunction:
+            function (value) { return value > 15; } */
+
+        for (const [key, value] of Object.entries(object)) {
+            if (predicateFunction(value)) {
+                return key
+            }
+        }
+        return undefined
+    },
+    drop(array, deleteCount) {
+
+        /* The drop method returns a given array with deleteCount
+            elements dropped from the beginning. */
+
+        // If deleteCount is not given, assume deleteCount = 1
+        if (deleteCount === undefined) {
+            deleteCount = 1
+        }
+
+        array.splice(0, deleteCount);
+        return array;
+
     }
 };
 
-console.log(_.pad('hello', 8));
+
 
 // Do not write or modify code below this line.
 module.exports = _;
